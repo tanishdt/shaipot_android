05d94a41ebfcddab0b68b1b9f183ffaf55a08d0f
Tanish Vijayvargiya <162412801+tanishdt@users.noreply.github.com>
2024-10-31 13:53:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Update and upgrade the package list
-sudo apt update && sudo apt upgrade -y
-
-# Install essential packages
-sudo apt install -y build-essential pkg-config libssl-dev git curl
-
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Source the Cargo environment to update the current shell session
-source $HOME/.cargo/env
-
 # Clone the Shaipot repository
 git clone https://github.com/shaicoin/shaipot.git
 
